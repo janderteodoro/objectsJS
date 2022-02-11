512684ab0fff65c1f4600e343822c7e9eb60540f
Jander Teodoro <79011974+janderteodoro@users.noreply.github.com>
2022-02-11 08:36:49 -0300

    using new with constructor function

---
+++ ES5/new.js
@@ -0,0 +1,5 @@
+let a = new Object();  // cria Objeto Vazio mesmo que {}
+let b = new Array(); // Cria array vazio mesmo que []
+let d = new Date(); // Cria objeto Date representando a hora atual
+let r = new RegExp("js"); // Cria uma RegExp
+
