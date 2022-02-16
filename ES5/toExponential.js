9c547db7bf1a367a59758bbc3bfafdc507308211
Jander Teodoro <79011974+janderteodoro@users.noreply.github.com>
2022-02-16 08:18:14 -0300

    toPrecision and toExponential methods

---
+++ ES5/toExponential.js
@@ -0,0 +1,4 @@
+// converte o número em uma string usando notação exponencial
+
+const n =  331318398409184902349349422409209242094782974892479281; 
+const j = n.toExponential(); // 3.313183984091849e+53
---
+++ ES5/toPrecision.js
@@ -0,0 +1,5 @@
+// converte o número em uma string com o números de dígitos significatvios
+
+const n = 23; 
+const nn = n.toPrecision(3); // 23.0
+
